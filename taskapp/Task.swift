c0f463476f63fe52419bf31511a1e84e329f1c40
miurakazuma67 <miurakazuma67@gmail.com>
2020-08-17 03:03:22 +0900

    pod installと.gitignoreの作成まで実施

---
+++ taskapp/Task.swift
@@ -0,0 +1,28 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 三浦　一真 on 2020/08/16.
+//  Copyright © 2020 Kazuma Miura. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
